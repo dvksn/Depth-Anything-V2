d622fc46ffab296829e29296b8919f6b37cd58e3
Sonali Mittal <sonalimittal@microsoft.com>
2024-07-29 14:19:39 +0530

    load state dict

--- metric_depth/inference.py
+++ metric_depth/inference.py
@@ -18,10 +18,11 @@ dataset = 'hypersim' # 'hypersim' for indoor model, 'vkitti' for outdoor model
 max_depth = 20 # 20 for indoor model, 80 for outdoor model
 
 model = DepthAnythingV2(**{**model_configs[encoder], 'max_depth': max_depth})
-for name, param in model.named_parameters():
-    print(name, param.shape)
+# for name, param in model.named_parameters():
+#     print(name, param.shape)
 
-# model.load_state_dict(torch.load(f'checkpoints/depth_anything_v2_metric_{dataset}_{encoder}.pth', map_location='cpu'))
+state_dict =  torch.hub.load_state_dict_from_url('https://huggingface.co/depth-anything/Depth-Anything-V2-Metric-Hypersim-Small/resolve/main/depth_anything_v2_metric_hypersim_vits.pth', map_location='cpu') 
+model.load_state_dict(state_dict)
 # model.eval()
 
 # raw_img = cv2.imread('your/image/path')
